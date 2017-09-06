096e94ed8ed19dd486ed7f26a74a555de718de96
Roda <2017@Roda.Wang>
2017-09-06 15:43:36 +0800

    Modify dashboard

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit }      from '@angular/core';
 
+import { Http }                   from '@angular/http'; 
+import 'rxjs/add/operator/toPromise';
+
 import { Hero }                   from './hero';
 import { HeroService }            from './hero.service';
 
@@ -10,13 +13,23 @@ import { HeroService }            from './hero.service';
 })
 
 export class DashboardComponent implements OnInit {
+
+  test: Hero[] = [];
+
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private http:Http) {
 
   }
 
   ngOnInit() :void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+
+    this.http.get(`app/heroes`)
+        .toPromise()
+        .then(response => {
+          this.test = response.json().data;
+          console.log(this.test);
+        });
   }
 }
